567fc004eef677532ef5b6667094627b53a1777a
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-20 03:54:18 -0400

    add fonts & landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-     
+        <div id="wave-container">
+          <div id="wave">
+          </div>
+        </div>
+     <LandingPage/>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div className="landing-container">
+            <div className="name-container">
+                <div className="name">Liz</div>
+                <div className="name" id="last">Moy</div>
+            </div>
+        </div>
+    )
+}
