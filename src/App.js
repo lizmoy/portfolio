4429b75e78bc2daa66491f337f26ed134d05e771
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-20 08:38:28 -0400

    build about component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
 import Projects from './components/Projects';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
      <LandingPage/>
      <Projects/>
+     <About/>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="about" id="about">
+            <h3>Hi I'm Liz!</h3>
+            <p className="about-text">I’m a Web Developer and Visual Designer interested in programming for a more equitable future. With a background in visual arts and community organizing, I bring a strong design sensibility and a wheelhouse of soft skills to the work that I do. My experience working with communities, particularly communities of color in New York City, has strengthened my knack for identifying problems and working through issues of accountability, privacy and engagement. With social impact always in mind, I hope to be building beautiful, useful, and accessible web applications.</p>
+        </div>
+    )
+}
