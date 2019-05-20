97d542d3029b84dd7465b949afb2672d3e709506
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-20 08:59:16 -0400

    fix linkedIn link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <div className="contact" id="contact">
             <h3>Let's connect</h3>
             <a className="contact-link" href="mailto:elizabethtmoy@gmail.com" target="blank">Email</a>
-            <a className="contact-link" href="www.linkedin.com/in/liz-moy" target="blank">LinkedIn</a>
+            <a className="contact-link" href="https://www.linkedin.com/in/liz-moy" target="blank">LinkedIn</a>
         </div>
     )
 }
