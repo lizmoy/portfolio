72329c70fa9d759b07af8f1ecaf12b5fa9a45a24
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-22 16:13:35 -0400

    wrap project images in links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,36 +15,32 @@ export default function Projects() {
         <div className="projects">
             <div className="row1">
                 <div className="project-div">
-                    <img src={popaholic} alt="" className="project"/>
+                <a className="project-text-link" href="https://lizmoy.github.io/popaholic/" target="blank"><img src={popaholic} alt="" className="project"/></a>
                     <div className="project-text">
                         <p>A desktop browser game built with HTML, CSS, and vanilla JavaScript</p>
-                        <a className="project-text-link" href="https://lizmoy.github.io/popaholic/" target="blank">Play the game</a>
                         <a className="project-text-link" href="https://github.com/lizmoy/popaholic" target="blank">Github</a>
                     </div>
                 </div>
                 <div className="project-div">
-                    <img src={lexicon} alt="" className="project"/>
+                <a className="project-text-link" href="https://lm-modern-lexicon.herokuapp.com/" target="blank"><img src={lexicon} alt="" className="project"/></a>
                     <div className="project-text">
                         <p>A React app using data from the Merriam Webster API and NewsAPI</p>
-                        <a className="project-text-link" href="https://lm-modern-lexicon.herokuapp.com/" target="blank">App</a>
                         <a className="project-text-link" href="https://github.com/lizmoy/dictionary-app" target="blank">Github</a>
                     </div>
                 </div>
             </div>
             <div className="row2">
                 <div className="project-div">
-                    <img src={iterator} alt="" className="project"/>   
+                <a className="project-text-link" href="https://iterator.surge.sh/" target="blank"><img src={iterator} alt="" className="project"/></a>   
                     <div className="project-text">
                         <p>A full stack app built in React and Express. Features: full CRUD and user authentication</p>
-                        <a className="project-text-link" href="https://iterator.surge.sh/" target="blank">App</a>
                         <a className="project-text-link" href="https://github.com/lizmoy/Iterator" target="blank">Github</a>
                     </div>
                 </div>
                 <div className="project-div">
-                    <img src={plantly} alt="" className="project"/>
+                <a className="project-text-link" href="https://plantly.surge.sh/" target="blank"><img src={plantly} alt="" className="project"/></a>
                     <div className="project-text">
                         <p>A full stack app built in React and Rails. Features: full CRUD and user authentication</p>
-                        <a className="project-text-link" href="https://plantly.surge.sh/" target="blank">App</a>
                         <a className="project-text-link" href="https://github.com/lizmoy/plantly" target="blank">Github</a>
                     </div>
                 </div>  
